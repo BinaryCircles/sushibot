a40d4d08611180e8ffcb8b16a351926d6279cd61
Aryan Jha <aryan@jha.io>
2019-06-02 06:59:33 -0700

    hall of fame only nominates a post once

--- plugins/hof.js
+++ plugins/hof.js
@@ -2,6 +2,7 @@ const bot = require('../bot.js')
 const fs = require('fs')
 const reactionHandler = require('./reactionHandler.js')
 let config = JSON.parse(fs.readFileSync('config.json'))
+let hofList = []
 
 if (reactionHandler.reaction.count >= config.hofCount) {
 
@@ -11,39 +12,52 @@ if (reactionHandler.reaction.count >= config.hofCount) {
   let hofMessage = reactionHandler.reaction.message.content
   let messageID = reactionHandler.reaction.message.id
 
-  bot.client.guilds.get(config.currentGuild).channels.get(config.hofChannel).send(
-    {
-      embed: {
-        author: {
-          name: "Hall of Fame",
-        },
-        thumbnail: { url: userAvatar },
-        color: 0xffbaf9,
-        fields: [
-          {
-            name: "User",
-            value: `<@${userID}>`,
-            inline: true
+  if (fs.existsSync("./plugins/hof.txt")) {
+    hofList = JSON.parse(fs.readFileSync("./plugins/hof.txt", 'utf8'))
+  } else {
+    fs.writeFileSync("./plugins/hof.txt", JSON.stringify(hofList), 'utf8')
+  }
+
+  if (hofList.includes(messageID)) {
+    return
+  } else {
+    bot.client.guilds.get(config.currentGuild).channels.get(config.hofChannel).send(
+      {
+        embed: {
+          author: {
+            name: "Hall of Fame",
           },
-          {
-            name: "Channel",
-            value: `<#${messageChannel}>`,
-            inline: true
-          },
-          {
-            name: "Message",
-            value: hofMessage,
-            inline: true
-          },
-          {
-            name: "ID",
-            value: messageID,
-            inline: true
-          }
-        ],
-        footer: { text: 'BinaryCircles/sushibot' }
+          thumbnail: { url: userAvatar },
+          color: 0xffbaf9,
+          fields: [
+            {
+              name: "User",
+              value: `<@${userID}>`,
+              inline: true
+            },
+            {
+              name: "Channel",
+              value: `<#${messageChannel}>`,
+              inline: true
+            },
+            {
+              name: "Message",
+              value: hofMessage,
+              inline: true
+            },
+            {
+              name: "ID",
+              value: messageID,
+              inline: true
+            }
+          ],
+          footer: { text: 'BinaryCircles/sushibot' }
+        }
       }
-    }
-  )
+    )
+
+    hofList.push(messageID)
+    fs.writeFileSync("./plugins/hof.txt", JSON.stringify(hofList), 'utf8')
+  }
 
 }
